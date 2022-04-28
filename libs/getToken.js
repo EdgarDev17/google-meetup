2625a9f266c7101b3a7f4a0691cc82d9d5869110
Edgardo Rodriguez <62724786+EdgarDev17@users.noreply.github.com>
2022-04-27 17:39:45 -0700

    server-side: I have updated getToken file, I have improved the code

--- libs/getToken.js
+++ libs/getToken.js
@@ -3,8 +3,8 @@
  */
 
 const twilio = require('twilio')
-const {AccessToken} = twilio.jwt.AccessToken // obtenemos el token desde twilio
-const {VideoGrant} = AccessToken// pedimos permisos para activar la camara
+const AccessToken = twilio.jwt.AccessToken // obtenemos el token desde twilio
+const { VideoGrant } = AccessToken// pedimos permisos para activar la camara
 
 // Configurando las variables de entorno siguiendo:  http://twil.io/secure
 const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
@@ -12,15 +12,14 @@ const twilioApiKey = process.env.TWILIO_API_KEY;
 const twilioApiSecret = process.env.TWILIO_API_SECRET;
 
 
-const generateToken = config => {
-    // NOTE: Puedo mandarle directamente las ENV variables
+const generateToken = () => {
     return new AccessToken(
-        config.twilio.accountSid,
-        config.twilio.apiKey,
-        config.twilio.apiSecret)
+        twilioAccountSid,
+        twilioApiKey,
+        twilioApiSecret)
 }
 
-const videoToken = (indentify, room, config) => {
+const videoToken = (identity, room) => {
     let videoGrant;
     if (typeof room !== 'undefined') {
         videoGrant = new VideoGrant({room});
@@ -28,7 +27,7 @@ const videoToken = (indentify, room, config) => {
         videoGrant = new VideoGrant();
     }
 
-    const token = generateToken(config);
+    const token = generateToken();
     token.addGrant(videoGrant);
     token.identity = identity;
     return token;
